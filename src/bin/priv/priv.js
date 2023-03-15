fe27a8b3680cc5baedd6ba7e967afe3e066cbd3e
NoelyGangello <noely.gangello@outlook.com>
2023-03-15 02:40:05 -0300

    new commit Wed, Mar 15, 2023  2:40:05 AM

--- src/bin/priv/priv.js
+++ src/bin/priv/priv.js
@@ -1,13 +1,12 @@
 /* Importações */
-const createError = require( "http-errors" );
-const express = require( "express" );
-const path = require( "path" );
-const cookieParser = require( "cookie-parser" );
-const logger = require( "morgan" );
-const ejs = require( "ejs" );
+const createError = require( "http-errors" ),
+	express = require( "express" ),
+	path = require( "path" ),
+	cookieParser = require( "cookie-parser" ),
+	logger = require( "morgan" );
 
-const indexRouter = require( "../../routes/index" );
-const usersRouter = require( "../../routes/users" ),
+const indexRouter = require( "../../routes/index" ),
+	usersRouter = require( "../../routes/users" ),
 	lyhTemps = require( "../../routes/noely-temps" );
 
 /* Variaveis */
