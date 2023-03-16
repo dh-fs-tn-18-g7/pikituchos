97d4a133d7ca3cfa3c6a4032f1331a66a368eed0
Arthur Roque <thurpacsecond@gmail.com>
2023-03-16 20:18:12 -0300

    finalização todas as páginas em mvc

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -1,6 +1,9 @@
 const ProductsController = {
     showProduto: (req, res) => {
         res.render('produto')
+    },
+    showCarrinho: (req, res) => {
+        res.render('auth/checkout')
     }
 }
 
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const ProductsController = require('../controllers/ProductsController')
 
-router.get('/produtos', ProductsController.showProdutos)
+router.get('/produtos', ProductsController.showProduto)
+router.get('/carrinho', ProductsController.showCarrinho)
 
 module.exports = router
+
