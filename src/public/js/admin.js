31af30b82b1d64d9a1e41c63509db770ed899c47
Bryan-Abranches <bryanabranches5@gmail.com>
2023-03-22 11:05:08 -0300

    Criação da logica da tela de admin

--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -1,82 +1,51 @@
-/* 
+console.log('start js')
 
-const btnInicio = document.querySelector("#inicio");
-const btnDadosPessoais = document.querySelector("#dadosPessoais");
-const btnMeusPedidos = document.querySelector("#meusPedidos");
-const btnMeusFavoritos = document.querySelector("#meusFavoritos");
-const btnInformacoes = document.querySelector("#informacoes");
-const btnTroca = document.querySelector("#trocarDevolver");
-const btnCartoes = document.querySelector("#cartoes");
+/* Botões */
 
-const homeInicio = document.querySelector(".boxCard");
-const dadosPessoais = document.querySelector(".dadosPessoais");
-const meusPedidos = document.querySelector(".meusPedidos");
-const meusFavoritos = document.querySelector(".meusFavoritos");
-const informacoes = document.querySelector(".informacoesDaConta");
-const trocarDevolver = document.querySelector(".trocarDevolver");
-const cartoes = document.querySelector(".cartoes");
+const produtos = document.querySelector('#btn-produtos');
+
+const litasProdutos = document.querySelector("#btn-listaProdutos");
+const cadastrarProdutos = document.querySelector("#btn-cadastrarPordutos");
+const pedidos = document.querySelector("#btn-pedidos");
+const config = document.querySelector("#btn-config");
+
+/* display */
+
+const displayProdutos = document.querySelector('#displayProdutos')
+
+console.log(displayProdutos)
+
+
+/* Eventos */
+
+produtos.addEventListener('click',exibirProduto)
+litasProdutos.addEventListener('click',exibirListaProduto)
+
+
+/* Funções */
+
 
 function exibirElemento(elemento) {
     elemento.style.display = "flex";
   }
   
   function ocultarElemento(elemento) {
-    elemento.style.display = "none";
-  }
-  
-  function exibirInicio() {
-    exibirElemento(homeInicio);
-    ocultarElemento(dadosPessoais);
-    ocultarElemento(meusFavoritos);
-    ocultarElemento(meusPedidos);
-    ocultarElemento(informacoes);
-    ocultarElemento(trocarDevolver);
-    ocultarElemento(cartoes);
-  }
-  
-  function exibirDadosPessoais() {
-    ocultarElemento(homeInicio);
-    exibirElemento(dadosPessoais);
-    ocultarElemento(meusFavoritos);
-    ocultarElemento(meusPedidos);
-    ocultarElemento(informacoes);
-    ocultarElemento(trocarDevolver);
-    ocultarElemento(cartoes);
-  }
-  
-  function exibirMeusPedidos() {
-    ocultarElemento(homeInicio);
-    ocultarElemento(dadosPessoais);
-    ocultarElemento(meusFavoritos);
-    exibirElemento(meusPedidos);
-    ocultarElemento(informacoes);
-    ocultarElemento(trocarDevolver);
-    ocultarElemento(cartoes);
-  }
-  
-  function exibirMeusFavoritos() {
-    ocultarElemento(homeInicio);
-    ocultarElemento(dadosPessoais);
-    exibirElemento(meusFavoritos);
-    ocultarElemento(meusPedidos);
-    ocultarElemento(informacoes);
-    ocultarElemento(trocarDevolver);
-    ocultarElemento(cartoes);
-  }
-  
-  function exibirInformacoes() {
-    ocultarElemento(homeInicio);
-    ocultarElemento(dadosPessoais);
-    ocultarElemento(meusFavoritos);
-    ocultarElemento(meusPedidos);
-    exibirElemento(informacoes);
-    ocultarElemento(trocarDevolver);
-    ocultarElemento(cartoes);
-  }
-  
-  function exibirTroca() {
-    ocultarElemento(homeInicio);
-    ocultarElemento(dadosPessoais);
+   
+    elemento.style.display = 'none';
 
+  }
+  
+  function exibirProduto() {
+   exibirElemento(displayProdutos)
 
-*/
+   
+
+}
+  
+  function exibirListaProduto() {
+    ocultarElemento(displayProdutos)
+
+   
+  }
+  
+  
