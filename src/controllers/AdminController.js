4ed11fa1e7c249787cde005b94d9d8f635e3e7db
Bryan-Abranches <bryanabranches5@gmail.com>
2023-03-26 19:38:42 -0300

    Atualização da logica de admin

--- src/controllers/AdminController.js
+++ src/controllers/AdminController.js
@@ -1,10 +1,43 @@
-const AdminController = { 
-    showLogin: (req,res)=>{
-        res.render('admin/login')
+/* Models */
+const databese = require("../database/db.json");
 
+/* Controller */
+
+const AdminController = {
+  showLogin: (req, res) => {
+    res.render("admin/login");
+  },
+
+  login: (req, res) => {
+    const user = databese.users;
+    const { email, password } = req.body;
+    console.log(password);
+    const userFound = user.find((user) => user.email === email); //
+    console.log(userFound);
+
+    if (!userFound) {
+      console.login("usuario invalido");
+      return;
     }
 
-}
+    const isValidPassword = userFound.password === password;
 
-module.exports = AdminController
+    console.log(isValidPassword);
 
+    if (!isValidPassword) {
+        console.log("E-mail ou senha invalidos");
+        return
+    }
+
+    console.log( userFound.isAdmin)
+   
+    if (userFound.isAdmin === false ) {
+        
+      return res.redirect("/");
+    }
+    
+    return res.redirect("/admin/home"); 
+  },
+};
+
+module.exports = AdminController;
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,14 +1,11 @@
-const express = require("express")
-const AdminController = require("../controllers/AdminController")
+const express = require("express");
+const AdminController = require("../controllers/AdminController");
 
-const router = express.Router()
+const router = express.Router();
 
-
-router.get('/admin/login',AdminController.showLogin);
-/* router.get('/admin/produtos',AdminController.showProdutos);
-router.get('/admin/listaProdutos',AdminController.showListaProdutos);
-router.get('/admin/produtos/cadastro',AdminController.showCadastrarProduto);
-router.get('/admin/pedidos',AdminController.showPedidos) */
+router.get("/admin/login", AdminController.showLogin);
+router.post("/admin/login", AdminController.login);
 
 
-module.exports = router
+
+module.exports = router;
