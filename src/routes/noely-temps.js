d57c30b3186d9859178771c1d625450ac345a356
Arthur Roque <thurpacsecond@gmail.com>
2023-03-28 12:07:44 -0300

    ajustes do pull

--- src/routes/noely-temps.js
+++ src/routes/noely-temps.js
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
-var express = require( "express" ),
-	router = express.Router();
 
-
-router.get( "/", ( l, y, h ) => {
-	y.render( "produto" );
-} );
-
-
-=======
 const express = require("express"),
   	  router = express.Router();
 
 router.get("/", (l, y, h) => {
   y.render("produto");
 });
->>>>>>> 13775de8207ad41721c90582a4b70ad1b4e9ab55
 
 /* [ exports ]
 =========================================== */
