b96c7ed05c43aa5b0daffdaf3ceb65ee67f8dcea
Arthur Roque <thurpacsecond@gmail.com>
2023-03-31 11:16:02 -0300

    ajustes tela cadastro

--- src/models/User.js
+++ src/models/User.js
@@ -42,7 +42,7 @@ module.exports = (sequelize, DataTypes) => {
           },
 
           rua: {
-            type: DataTypes.STRING(50),
+            type: DataTypes.STRING(100),
             allowNull: false
           },
     
