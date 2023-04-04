4fdad034ace7e8ec374bf15a939735efa8e491f1
Bryan-Abranches <bryanabranches5@gmail.com>
2023-04-04 18:50:28 -0300

    Atualização final da telas de user - sem a logica

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -8,7 +8,7 @@ const UserController = require('../controllers/UsersControler')
 router.get('/user',UserController.showUser)
 router.get('/user/dadosPessoais', UserController.ShowUserDadosPessoais)
 router.get('/user/pedidos', UserController.ShowUserPedidos)
-//router.get('/user/infoConta', UserController.ShowUserInfoConta)
+router.get('/user/infoConta', UserController.ShowUserInfoConta)
 router.get('/user/card', UserController.ShowUserCard)
 
 
